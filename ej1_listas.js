271bb4df465877b81c0e89fb5566b6f0677ad8a5
celia hidalgo <120373819+celiahidalgoo@users.noreply.github.com>
2023-01-17 13:20:36 +0100

    Update ej1_listas.js

--- ej1_listas.js
+++ ej1_listas.js
@@ -6,7 +6,15 @@ numeros.push(i);
 i = i + 2 ;
 
 }
+console.log("Lista de numeros: "+ numeros);
     // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+        var numerosImpares = []
+    for(var index = 1; index<=19; index++){
+        if(index % 2 == 1 ){
+        numerosImpares. push(index)
+            listasImpares.push(i);
+        }
+    }
     
     // Crea una lista de comida.
     
